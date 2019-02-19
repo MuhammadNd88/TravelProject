6133828104fcd113d7c1e2aa6c24e1fdb4027a24
MuhammadNd88 <46718678+MuhammadNd88@users.noreply.github.com>
2019-02-19 14:55:27 -0700

    changed

--- JavaScript/vocation.js
+++ JavaScript/vocation.js
@@ -83,7 +83,8 @@ var carTimer = setInterval(catWalk, 1);
 //=================================================================================image section=======================================================================//
 var pageBody = document.getElementsByTagName("div")[5];
 var newImg = document.createElement("img");
-newImg.src = "C:/Users/811192/Desktop/JavaScript/FinalDay/Images/img1.jpg ";
+newImg.src =
+  "C:/Users/811192/Desktop/JavaScript/FinalDay/TravelProject/Images/img1.jpg ";
 newImg.style.border = "1px solid black";
 newImg.style.backgroundRepeat = "no-repeat";
 newImg.style.backgroundSize = "cover";
